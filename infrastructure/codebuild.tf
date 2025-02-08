1d9f5e74782fbcbe594db901523bbb55b04ef2bf
Daniel Aguilera <daniel.aguilera.garcia.1991@gmail.com>
2025-02-08 17:56:47 +0100

    minor change

--- infrastructure/codebuild.tf
+++ infrastructure/codebuild.tf
@@ -31,7 +31,7 @@ resource "aws_codebuild_project" "eks_deploy" {
 
   environment {
     compute_type    = "BUILD_GENERAL1_SMALL"
-    image           = "aws/codebuild/amazonlinux2-x86_64-standard:4.0"
+    image           = "aws/codebuild/standard:7.0"  # ✅ Uses an AWS-managed image with Python 3.11
     type            = "LINUX_CONTAINER"
     privileged_mode = true
   }

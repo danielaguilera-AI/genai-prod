fbc9668acc04e83f6181f78e7926b211f711a29a
Daniel Aguilera <daniel.aguilera.garcia.1991@gmail.com>
2025-02-20 13:18:22 +0100

    minor

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,5 +1,10 @@
 output "ecr_repository_url" {
-  value = try(aws_ecr_repository.llm_repository[0].repository_url, "Repository does not exist yet")
+  value = try(
+    aws_ecr_repository.llm_repository[0].repository_url,
+    data.aws_ecr_repository.existing_repository.repository_url,
+    "repository-not-found"
+  )
 }
+
 
 

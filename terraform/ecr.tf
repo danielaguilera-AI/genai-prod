ae5295149d5c611f335a69f36b25679cd91a8014
Daniel Aguilera <daniel.aguilera.garcia.1991@gmail.com>
2025-02-20 12:27:29 +0100

    feat: to avoid recreate existing ecr repo

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -1,3 +1,16 @@
-resource "aws_ecr_repository" "llm_repository" {
+# Check if the ECR repository already exists
+data "aws_ecr_repository" "existing_repository" {
   name = "data-science/llm-deployment"
+}
+
+# Create the ECR repository only if it does not exist
+resource "aws_ecr_repository" "llm_repository" {
+  count = length(data.aws_ecr_repository.existing_repository.id) > 0 ? 0 : 1
+  name  = "data-science/llm-deployment"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  image_tag_mutability = "MUTABLE"
 }

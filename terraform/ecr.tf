03a12c0644c68961febe7b9a912f0248919febe5
Daniel Aguilera <daniel.aguilera.garcia.1991@gmail.com>
2025-02-20 13:05:40 +0100

    minor

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -1,16 +1,9 @@
-# Try to fetch an existing ECR repository
 data "aws_ecr_repository" "existing_repository" {
   name = "data-science/llm-deployment"
 }
 
-# Create the repository if it does not exist
 resource "aws_ecr_repository" "llm_repository" {
-  name = "data-science/llm-deployment"
-
-  image_scanning_configuration {
-    scan_on_push = true
-  }
-
-  image_tag_mutability = "MUTABLE"
+  count = length(data.aws_ecr_repository.existing_repository.id) > 0 ? 0 : 1
+  name  = "data-science/llm-deployment"
 }
 
--- terraform/output.tf
+++ terraform/output.tf
@@ -1,4 +1,5 @@
 output "ecr_repository_url" {
-  value = aws_ecr_repository.llm_repository.repository_url
+  value = try(aws_ecr_repository.llm_repository[0].repository_url, "Repository does not exist yet")
 }
+
 

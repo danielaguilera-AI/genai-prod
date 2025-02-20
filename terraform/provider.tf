804e7fb1c5ecdebb16a4cb1c401afc3c51c61db8
Daniel Aguilera <daniel.aguilera.garcia.1991@gmail.com>
2025-02-20 11:20:22 +0100

    feat: added an S3 bucket for terraform state bakend

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -7,6 +7,12 @@ terraform {
       version = "~> 5.87.0"
     }
   }
+  backend "s3" {
+      bucket         = "aws-s3-demo-terraform-backend"
+      key            = "terraform/state.tfstate"
+      region         = "us-east-1"
+      encrypt        = true
+  }
 }
 
 provider "aws" {

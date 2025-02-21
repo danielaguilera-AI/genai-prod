f098e4eeab2fa5631c5eeeabab90f8471d299c64
Daniel Aguilera <daniel.aguilera.garcia.1991@gmail.com>
2025-02-21 10:26:23 +0100

    fix: solve issue

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -1,11 +1,7 @@
-data "aws_ecr_repository" "existing_repository" {
+resource "aws_ecr_repository" "llm_repository" {
   name = "data-science/llm-deployment"
 }
 
-resource "aws_ecr_repository" "llm_repository" {
-  count = length(data.aws_ecr_repository.existing_repository.repository_url) > 0 ? 0 : 1
-  name  = "data-science/llm-deployment"
-}
 
 
 
--- terraform/output.tf
+++ terraform/output.tf
@@ -1,8 +1,5 @@
 output "ecr_repository_url" {
-  value = coalesce(
-    try(data.aws_ecr_repository.existing_repository.repository_url, ""),
-    try(aws_ecr_repository.llm_repository[0].repository_url, "")
-  )
+  value = aws_ecr_repository.llm_repository.repository_url
 }
 
 
